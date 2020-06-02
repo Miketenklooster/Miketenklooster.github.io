49aa7f57f33216da1080dbe398a94b142b8d97ae
Miketenklooster <mike@ten-klooster.com>
2020-06-02 11:30:42 +0200

    Index

---
+++ dist/js/omdb.js
@@ -0,0 +1,95 @@
+const url = 'https://www.omdbapi.com/';
+const key = 'e94fc62a';
+
+const type = {
+    all: '',
+    movie: 'movie',
+    series: 'series',
+    episode: 'episode'
+};
+
+function getView(elementId, view, page) {
+    sessionStorage.setItem('site', view);
+    sessionStorage.setItem('page', page);
+    return fetch(view /*, options */)
+        .then((response) => response.text())
+        .then((html) => {
+            const content = document.getElementById(`${elementId}`);
+            content.innerHTML = html;
+            let scripts = content.getElementsByTagName("script");
+            for (let i = 0; i < scripts.length; i++) {
+                eval(scripts[i].innerText);
+            }
+        })
+        .catch((error) => console.error(error))
+}
+
+function prevPage()
+{
+    let current_page = Number(sessionStorage.getItem("page"));
+    if (current_page > 1) {
+        current_page--;
+        getView('main-content', 'views/home.html', current_page);
+    }
+}
+
+function nextPage(totalResults)
+{
+    let current_page = Number(sessionStorage.getItem("page"));
+    if (current_page < numPages(totalResults)) {
+        current_page++;
+        getView('main-content', 'views/home.html', current_page);
+    }
+}
+
+function numPages(totalResults)
+{
+    return Math.ceil(totalResults / 9);
+}
+
+// For code optimisation
+//
+// function getAll() {
+//     fetch(url + "?s=" + sessionStorage.getItem('title') + "&apikey=" + key, {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then((response) => response.json())
+//         .then((json) => {
+//             const data = json["Search"];
+//             return data.map((data) => {
+//                 console.log(data);
+//             });
+//         })
+//         .catch((error) => console.error(error))
+// }
+//
+// function getDetails(id) {
+//     sessionStorage.setItem("omdbID", id);
+//     return fetch(url + "?i=" + id + "&plot=full&apikey=" + key, {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then((response) => response.json())
+//         .catch((error) => console.error(error))
+// }
+//
+// function searchData(title, type) {
+//     sessionStorage.setItem("title", title);
+//     return fetch(url + "?s=" + title + "&type=" + type + "&apikey=" + key, {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then((response) => response.json())
+//         .then((json) => {
+//             json.map(json => json["Search"]);
+//             console.log(json);
+//         })
+//         .catch((error) => console.error(error))
+// }
